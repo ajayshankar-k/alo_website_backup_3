1ca1b75a909e793b30cf827a6d4eaae8d0bd6bb3
Ajay <108935515+ajayshankar-k@users.noreply.github.com>
2024-03-18 14:31:38 +0530

    changed dev url

--- src/pages/api/BaseUrl.js
+++ src/pages/api/BaseUrl.js
@@ -2,7 +2,9 @@ import React from 'react'
 import axios from 'axios'
 
 const BaseUrl = axios.create({
-    baseURL:'https://aloinfotech.in/api/api',
+
+    // baseURL:'https://aloinfotech.in/api/api',
+    baseURL:'http://ec2-15-206-174-128.ap-south-1.compute.amazonaws.com:3000/api', //devurl
     headers:{
         'Content-Type': 'application/json',
     }
